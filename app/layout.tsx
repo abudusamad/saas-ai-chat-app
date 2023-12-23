16119fb8c5109aca727f589b090475f155e97149
abudusamad <abudusamed@gmail.com>
2023-12-23 01:41:08 +0000

    Adding the clerk authentication to the project

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,34 +1,35 @@
-import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
-import './globals.css'
-import { SiteCongfig } from '@/config/siteConfig'
+import { SiteCongfig } from "@/config/siteConfig";
+import { ClerkProvider } from "@clerk/nextjs";
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
 
-
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: {
-    default: SiteCongfig.title,
-    template: '%s | ' + SiteCongfig.title,
-  },
-  description: SiteCongfig.description,
-  icons: [
-    {
-      url: "/logo.svg",
-      href:"/logo.svg"
-    }
-  ]
-
-}
+	title: {
+		default: SiteCongfig.title,
+		template: "%s | " + SiteCongfig.title,
+	},
+	description: SiteCongfig.description,
+	icons: [
+		{
+			url: "/logo.svg",
+			href: "/logo.svg",
+		},
+	],
+};
 
 export default function RootLayout({
-  children,
+	children,
 }: {
-  children: React.ReactNode
+	children: React.ReactNode;
 }) {
-  return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
-    </html>
-  )
+	return (
+		<ClerkProvider>
+			<html lang="en">
+				<body className={inter.className}>{children}</body>
+			</html>
+		</ClerkProvider>
+	);
 }
