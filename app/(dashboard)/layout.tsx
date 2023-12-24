8662a438a97b9ddb84bdbcfec2d7505f950408f3
abudusamad <abudusamed@gmail.com>
2023-12-24 18:33:53 +0000

    finished witht the landing page

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -1,10 +1,11 @@
-import { Sidebar } from "lucide-react";
+import Sidebar from "./_components/sidebar";
+
 
 const DashboardLayout = ({ children }: { children: React.ReactNode }) => {
 	return (
 		<div className="h-full relative">
             <div className="hidden h-full md:flex md:flex-col md:w-72 md:fixed md:inset-y-0 z-80 bg-gray-900">
-                <Sidebar/>
+				<Sidebar/>
                 
             </div>
 			<main>{children}</main>
--- app/(landing)/page.tsx
+++ app/(landing)/page.tsx
@@ -1,11 +1,13 @@
 import { LandingHero } from "./_components/landing-hero";
 import { LandingNavbar } from "./_components/landing-navbar";
+import { LandingTestimonials } from "./_components/landing-testimonial";
 
 const LandingPage = () => {
     return ( 
         <div className="h-full">
             <LandingNavbar />
-            <LandingHero/>
+            <LandingHero />
+            <LandingTestimonials/>
         </div>
      );
 }
