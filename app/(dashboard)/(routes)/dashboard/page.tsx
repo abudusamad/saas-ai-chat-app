37d5a75822d58fcfbac8c3c78e0a185be8b9a1cd
abudusamad <abudusamed@gmail.com>
2023-12-24 23:59:10 +0000

    making the dashboard for the page

--- app/(dashboard)/(routes)/dashboard/page.tsx
+++ app/(dashboard)/(routes)/dashboard/page.tsx
@@ -1,3 +1,39 @@
+"use client";
+
+import { Card } from "@/components/ui/card";
+import { tools } from "@/config/constant";
+import { cn } from "@/lib/utils";
+import { ArrowRight } from "lucide-react";
+import { useRouter } from "next/navigation";
 export default function Home() {
-	return <div className="text-6xl text-emerald-500">Hello world</div>;
+	const router = useRouter();
+	return (
+		<div>
+			<div className="mb-8 space-y-4">
+				<h2 className="text-2xl md:text-4xl font-bold text-center">
+					Explore the power of AI
+				</h2>
+				<p className="text-muted-foreground font-light text-sm md:text-lg text-center">
+					Chat with the smartest AI on the planet. Generate content
+				</p>
+			</div>
+			<div className="px-4 md:px-20 lg:px-32 space-y-4">
+				{tools.map((tool) => (
+					<Card
+						onClick={() => router.push(tool.href)}
+						key={tool.href}
+						className="flex items-center justify-between p-4 border-black/5 hover:shadow-md transition cursor-pointer"
+					>
+						<div className="flex items-center gap-x-4">
+							<div className={cn("p-2 w-fit rounded-md", tool.bgColor)}>
+								<tool.icon className={cn("w-8 h-8", tool.color)} />
+							</div>
+							<div className="font-semibold">{tool.label}</div>
+						</div>
+						<ArrowRight className="w-6 h-6" />
+					</Card>
+				))}
+			</div>
+		</div>
+	);
 }
