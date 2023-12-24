ed63f228f444c08f3723610f602a5b15079b0639
abudusamad <abudusamed@gmail.com>
2023-12-24 17:01:15 +0000

    Making the topbar navabar

--- app/(landing)/_components/landing-navbar.tsx
+++ app/(landing)/_components/landing-navbar.tsx
@@ -1,17 +1,24 @@
+"use client";
+
 import Logo from "@/components/Logo"
+import { Button } from "@/components/ui/button"
 import Link from "next/link"
 
+import { useAuth } from "@clerk/nextjs"
+
 export const LandingNavbar = () => {
+    const { isSignedIn } = useAuth();
+
     return (
         <nav className="flex items-center justify-between p-4 bg-transparent">
             <Link href="/" className="flex items-center">
                 <Logo/>
             </Link>
             <div className="flex items-center ">
-                <Link href="/sign-up">
-                    <button>
+                <Link href={isSignedIn ? "/dashboard" : "sign-up"}>
+                    <Button variant="outline" className="rounded-full">
                         Get Started
-                    </button>
+                    </Button>
                     
                 </Link>
             </div>
--- app/(landing)/layout.tsx
+++ app/(landing)/layout.tsx
@@ -1,7 +1,7 @@
 const LandingLayout = ({ children }: { children: React.ReactNode }) => {
 	return (
 		<main className="h-full bg-[#111827] overflow-auto">
-			<div className="mx-auto max-w-screen-xl w-full">{children}</div>
+			<div className="mx-auto max-w-screen-6xl w-full">{children}</div>
 		</main>
 	);
 };
