ed66d130a3aab8917175cd7565a7f80bcb3b24f0
abudusamad <abudusamed@gmail.com>
2023-12-24 22:56:27 +0000

    Making the sidebar for the page

---
+++ app/(dashboard)/_components/sidebar-item.tsx
@@ -0,0 +1,32 @@
+"use client";
+
+import { cn } from "@/lib/utils";
+import { LucideIcon } from "lucide-react";
+import { usePathname } from "next/navigation";
+import path from "path";
+
+interface SidebarItemProps {
+	label: string;
+	icon: LucideIcon;
+    color?: string;
+    href?: string;
+}
+const SidebarItem = ({ label, icon: Icon, color,href }: SidebarItemProps) => {
+	const pathname = usePathname();
+
+	return (
+		<div
+			className={cn(
+				"text-sm group p-3 w-full justify-start cursor-pointer hover:text-white hover:bg-white/10 rounded-lg transition",
+				pathname === href ? "text-white bg-white/10" : "text-zinc-400",
+			)}
+		>
+			<div className="flex items-center flex-1">
+				<Icon className={cn("h-6 w-6 mr-3", color)} />
+				<span>{label}</span>
+			</div>
+		</div>
+	);
+};
+
+export default SidebarItem;
--- app/(dashboard)/_components/sidebar.tsx
+++ app/(dashboard)/_components/sidebar.tsx
@@ -1,16 +1,86 @@
+"use client";
+
 import Logo from "@/components/Logo";
+import {
+	Code,
+	ImageIcon,
+	LayoutDashboard,
+	MessageSquare,
+	Music,
+	Settings,
+	VideoIcon,
+} from "lucide-react";
 import Link from "next/link";
+import SidebarItem from "./sidebar-item";
+
+export const routes = [
+	{
+		label: "Dashboard",
+		href: "/dashboard",
+		icon: LayoutDashboard,
+		color: "text-sky-700",
+	},
+	{
+		label: "Conversation",
+		href: "/conversation",
+		icon: MessageSquare,
+		color: "text-violet-500",
+	},
+	{
+		label: "Image Generation",
+		href: "/image",
+		icon: ImageIcon,
+		color: "text-pink-700",
+	},
+	{
+		label: "Video Generation",
+		icon: VideoIcon,
+		color: "text-orange-700",
+		href: "/video",
+	},
+	{
+		label: "Music Generation",
+		icon: Music,
+		color: "text-emerald-500",
+		href: "/music",
+	},
+	{
+		label: "Code Generation",
+		icon: Code,
+		color: "text-green-700",
+		href: "/code",
+	},
+	{
+		label: "Settings",
+		icon: Settings,
+		href: "/settings",
+	},
+];
 
 const Sidebar = () => {
-    return (<div className="h-full flex flex-col  text-white space-y-4 py-4">
-        <div className="px-3 py-2 flex-1">
-            <Link href="/dashboard" className="flex items-center pl-3 mb-14">
-                <div>
-                    <Logo/>
-                </div>
-            </Link>
-        </div>
-    </div> );
-}
- 
+	return (
+		<div className="h-full flex flex-col  text-white space-y-4 py-4">
+			<div className="px-3 py-2 flex-1">
+				<Link href="/dashboard" className="flex items-center pl-3 mb-14">
+					<div>
+						<Logo />
+					</div>
+				</Link>
+				<div className="spacy-y-1">
+					{routes.map((route) => (
+						<Link key={route.href} href={route.href}>
+							<SidebarItem
+								icon={route.icon}
+								label={route.label}
+								color={route.color}
+								href={route.href}
+							/>
+						</Link>
+					))}
+				</div>
+			</div>
+		</div>
+	);
+};
+
 export default Sidebar;
