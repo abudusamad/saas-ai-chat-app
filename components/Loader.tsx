068e04470d2fdf9cea7ba4a8158f1db08b6a310f
abudusamad <abudusamed@gmail.com>
2023-12-25 23:56:55 +0000

    working on the loading state

--- components/Loader.tsx
+++ components/Loader.tsx
@@ -2,10 +2,15 @@ import Image from "next/image";
 
 const Loader = () => {
 	return (
-		<div>
-			<div>
+		<div className="h-full flex flex-col gap-y-4 items-center justify-center">
+			<div className="w-10 h-10 relative animate-spin">
 				<Image fill alt="logo" src="/logo.svg" />
-				<div>Mastech is thinking ...</div>
+				<div className="text-sm text-muted-foreground">
+					Mastech is thinking 
+					<span className="animate-dots">.</span>
+					<span className="animate-dots delay-200">.</span>
+					<span className="animate-dots delay-400">.</span>
+				</div>
 			</div>
 		</div>
 	);
